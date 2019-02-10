00092862ab41946ad7e384ca4f840ed7211ed013
grimgoon <alexander@hermansson.us>
2019-02-10 12:51:11 +0100

    Created VSCode Kanban Board & Started on the Upload Image Feature

---
+++ frontend/src/components/Admin/UploadImage/UploadImage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './UploadImage.module.css'
+
+const UploadImage = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default UploadImage;
--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './Footer.module.css';
 
+// Temporarily add Admin Upload Button to footer to try to upload images
+import UploadImage from '../Admin/UploadImage/UploadImage';
+
 const Footer = () => (
-    <div className={styles.footer}>hej</div>
-)
+    <div className={styles.footer}>
+        <UploadImage/>
+    </div>
+) 
 
 export default Footer;
