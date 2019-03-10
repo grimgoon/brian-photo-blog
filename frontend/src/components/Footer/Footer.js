4401f29d925c77d6555c0b3ea692c2ad5ae437d5
grimgoon <alexander@hermansson.us>
2019-03-10 22:35:34 +0100

    Updated footer with example content

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -3,6 +3,9 @@ import styles from './Footer.module.css';
 
 const Footer = () => (
     <div className={styles.footer}>
+        <div className={styles.email}>info@1-foto.com</div>
+        <div className={styles.rights}>1-Foto. all rights reserved ©</div>
+        <div className={styles.credits}>Made by: <a href="https://github.com/grimgoon/">Alexander Hermansson</a></div>
     </div>
 ) 
 
